b25e17903725ea0c60659031c760875ea0c8784d
F.Paskalev <77507389+FilipPaskalev@users.noreply.github.com>
2022-03-25 18:55:22 +0000

    update

--- install-ts-boilerplate.bat
+++ install-ts-boilerplate.bat
@@ -6,49 +6,59 @@ ECHO =======================================
 ECHO  THIS INSTALLATION IS JUST FOR WINDOWS
 ECHO =======================================
 
-CALL mkdir ts-boilerplate && cd ts-boilerplate
+mkdir ts-boilerplate && cd ts-boilerplate
 ECHO STATUS: DIR ts-boilerplate create
 
-CALL :install_node
-CALL :install_typescript
-CALL :setup_all_files
+CALL :INSTALL_NODE
+CALL :INSTALL_TS
+CALL :INIT_TS_PROJECT
 
-PAUSE
+ECHO STATUS: Create style.css
+REM. > style.css
+ECHO STATUS: style.css file create successful 
 
-:install_node
-ECHO INSTALLATION: NodeJS
+ECHO STATUS: Create .gitignore file
+ECHO node_modules/ > .gitignore
+ECHO STATUS: .gitignore file create successful
+
+ECHO SETSTATUSUP: Create index.ts
+mkdir src && cd src
+ECHO console.log("Hello World!"); > index.ts && cd..
+ECHO STATUS: index.ts file create successful
+
+ECHO STATUS: Create index.html
+REM. > index.html
+ECHO ^<!doctype html^> >> index.html
+ECHO ^<html class="no-js" lang="en"^> >> index.html
+ECHO ^<head^> >> index.html
+ECHO   ^<meta charset="utf-8"^> >> index.html
+ECHO   ^<title^>Name of the webpage^</title^> >> index.html
+ECHO   ^<meta name="description" content="do some description"^> >> index.html
+ECHO   ^<meta name="viewport" content="width=device-width, initial-scale=1"^> >> index.html
+ECHO   ^<link rel="stylesheet" href="style.css"^> >> index.html
+ECHO ^</head^> >> index.html
+ECHO ^<body^> >> index.html
+ECHO   ^<!-- Add your site or application content here --^> >> index.html
+ECHO   ^<script^ src="src/index.js"^>^</script^> >> index.html
+ECHO ^</body^> >> index.html
+ECHO ^</html^> >> index.html
+ECHO STATUS: index.html file create successful
+
+ECHO STATUS: Build the project
+CALL tsc -build
+ECHO STATUS: Build successful
+
+:INSTALL_NODE
+ECHO STATUS: Install NodeJS
 CALL npm install npm && npm fund
 ECHO STATUS: NodeJS successful installed locally
-EXIT
 
-:install_typescript
-ECHO INSTALLATION: TypeScript
+:INSTALL_TS
+ECHO STATUS: Install TypeScript
 CALL npm install typescript && npm fund
 ECHO STATUS: TypeScript successful installed locally
-EXIT /B 0
 
-:create_project
-ECHO SETUP: Initializate TypeScript project
+:INIT_TS_PROJECT
+ECHO STATUS: Initializate TypeScript project
 CALL tsc --init
 ECHO STATUS: Initialization finish successful
-EXIT /B 0
-
-:setup_all_files
-ECHO SETUP: Create index.ts
-CALL mkdir src && cd src
-CALL ECHO console.log("Hello World!"); > index.ts && cd..
-ECHO STATUS: file create 
-
-ECHO SETUP: Create style.css
-CALL REM. > style.css
-ECHO STATUS: file create 
-
-ECHO SETUP: Create index.html
-CALL ECHO <!doctype html>\n<html class=\"no-js\" lang=\"en\">\n<head>\n  <meta charset=\"utf-8\">\n  <title>Name of the webpage</title>\n  <meta name=\"description\" content=\"do some description\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n  <link rel=\"stylesheet\" href=\"style.css\">\n</head>\n<body>\n  <!-- Add your site or application content here -->\n  <script src=\"src/index.js\"></script>\n</body>\n</html> > index.html
-ECHO STATUS: file create 
-
-ECHO SETUP: .gitignore file
-CALL ECHO node_modules/ > .gitignore
-ECHO STATUS: file create
-
-PAUSE
